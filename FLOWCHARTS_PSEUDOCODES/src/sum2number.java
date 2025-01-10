82190c0e0e3c6d4978ebb0195b47362625aa402c
RAJESH-IPSEC <rajeshkumarnayakwork@gmail.com>
2025-01-10 18:17:07 +0530

    Sum 2 number java file added

---
+++ FLOWCHARTS_PSEUDOCODES/src/sum2number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class sum2number {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int Sum,a,b;
+        System.out.println("Enter 2 number to sum");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        Sum = a + b;
+        System.out.println("Sum is: " + Sum);
+    }
+}
