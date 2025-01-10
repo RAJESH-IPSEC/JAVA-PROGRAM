8e684062a1f82b61933b4dbe722359a1f95e526b
RAJESH-IPSEC <rajeshkumarnayakwork@gmail.com>
2025-01-10 17:42:26 +0530

    delete

--- show_name.java
+++ show_name.java
@@ -1,6 +1,6 @@
 public class show_name {
     public static void main(String[] args){
         String name = "Rajesh";
-        System.out.println("Hello my name is:" + name);
+        System.out.println("Hello my name is:   " + name);
     }
 }
